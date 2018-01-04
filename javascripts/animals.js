49b2a6825539933c2032a639e215c78204c820c0
dhazlettjr <d.hazlett.jr@gmail.com>
2018-01-04 08:29:26 -0600

    rearranged my module and animals js file

--- javascripts/animals.js
+++ javascripts/animals.js
@@ -1,20 +1,35 @@
+'use strict';
+
 let carnivores = [];
 let herbivores = [];
 
-loadCarnivores = function(carnivores) {
+let loadCarnivores = function(callbackToInvoke) {
   const loader = new XMLHttpRequest();
 
   loader.addEventListener("load", function() {
     // Set the value of the private array
     carnivores = JSON.parse(this.responseText);
+    callbackToInvoke(carnivores);
 
     // Now exeute the callback function (`callbackToInvoke`) so that the caller knows that the process is complete. Make sure to pass the carnivore array as an argument.
   });
 
-  loader.addEventListener("load", function () {
-      herbivores = JSON.parse(this.responseText);
-  });
+  loader.open("GET", "carnivores.json");
+  loader.send();
 };
 
-module.exports = { loadCarnivores };
-module.exports = { loadHerbivores };
+  let loadHerbivores = function(callbackToInvoke) {
+      const loader = new XMLHttpRequest();
+
+  loader.addEventListener("load", function () {
+      herbivores = JSON.parse(this.responseText);
+      callbackToInvoke(herbivores);
+  });
+
+  loader.open("GET", "herbivores.json");
+  loader.send();
+  };
+
+
+module.exports = { loadCarnivores, loadHerbivores };
+
--- javascripts/module-xhr.js
+++ javascripts/module-xhr.js
@@ -1,11 +1,19 @@
+'use strict';
+
 const animal = require("./animals");
 
-function showCarnivores(carnivores) {
+let carnivoresBox = document.getElementById("carnivores");
+let herbivoresBox = document.getElementById("herbivores");
+
+function showCarnivores(animals) {
   // code that takes the carnovores array and displays it to the DOM
+carnivoresBox.innerHTML = carnivores;
+console.log(showCarnivores);
 }
 
-function showHerbivores(herbivores) {
-    
+
+function showHerbivores(animals) {
+
 }
 
 animals.loadCarnivores(showCarnivores);
